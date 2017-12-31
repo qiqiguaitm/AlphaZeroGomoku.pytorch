eb0901c3709e58eb64633035009dcf6be23fa16b
qiqiguaitm <qiqiguaitm@sina.com>
2017-12-31 23:31:57 +0800

    u

--- dist/data_server.py
+++ dist/data_server.py
@@ -6,6 +6,12 @@ This module builds on BaseHTTPServer by implementing the standard GET
 and HEAD requests in a fairly straightforward manner.
 
 """
+
+
+__version__ = "0.1"
+__author__ = "zhangtianming"
+__home_page__ = "http://qiqiguaitm.github.io"
+
 import os
 import posixpath
 import BaseHTTPServer
