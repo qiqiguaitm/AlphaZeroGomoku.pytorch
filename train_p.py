1ef91d2c7e969f97be9e3500b58bf5c870a893b2
qiqiguaitm <qiqiguaitm@sina.com>
2017-12-31 10:39:50 +0800

    u

--- train_p.py
+++ train_p.py
@@ -101,8 +101,8 @@ def policy_evaluate(gpu_id, win_queue, job_queue, job_queue_lock, game, role,
 class TrainPipeline():
     def __init__(self):
         # params of the board and the game
-        self.board_width = 19
-        self.board_height = 19
+        self.board_width = 8
+        self.board_height = 8
         self.n_in_row = 5
         self.board = Board(width=self.board_width, height=self.board_height, n_in_row=self.n_in_row)
         self.game = Game(self.board)
@@ -113,7 +113,7 @@ class TrainPipeline():
         self.n_playout = 400  # num of simulations for each move
         self.c_puct = 5
         self.buffer_size = 10000
-        self.batch_size = 2048  # mini-batch size for training
+        self.batch_size = 512  # mini-batch size for training
         self.data_buffer = deque(maxlen=self.buffer_size)
         self.play_batch_size = 1
         self.epochs = 5  # num of train_steps for each update
@@ -128,6 +128,8 @@ class TrainPipeline():
         self.num_inst = 0
         self.model_file = 'checkpoint.pth.tar'
         self.best_model_name = 'checkpoint_best.pth.tar'
+        if os.path.exists(self.model_file):
+            shutil.rmtree(self.model_file)
         self.manager = multiprocessing.Manager()
         # start training from a given policy-value net
         #        policy_param = pickle.load(open('current_policy.model', 'rb'))
@@ -242,7 +244,7 @@ class TrainPipeline():
                     item = self.data_queue.get()
                     self.data_buffer.append(item)
                     cnt = cnt + 1
-                    if cnt > 512:
+                    if cnt > 128:
                         break
                 t2 = time.time()
                 print("batch i:{}, data_queue_size:{},time_used:{:.3f}".format(i + 1, self.data_queue.qsize(), t2 - t1))
